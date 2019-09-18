6677efe10524c5a9ac8fad9974ccd91b49c6a955
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2019-09-18 20:31:20 +0200

    Creating the concrete Auth Repository

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+
+        }
+        public Task<User> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<User> Register(User user, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
