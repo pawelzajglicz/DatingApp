51c6de9c0bfdbb19ea3ac42d476c1573a33b8a63
Paweł Zajglicz <31182014+pawelzajglicz@users.noreply.github.com>
2019-10-13 00:47:57 +0200

    Client side login and register (#2)
    * Creating the User model
    
    * Adding interface for repository pattern for Auth Repository
    
    * Creating the concrete Auth Repository
    
    * Creating Register method
    
    * Fix wrong type of User.Username
    
    * Creating the Login repository method
    
    * Typo fix
    
    * Fixin migration files
    
    * Registering services in the Startup class
    
    * Creating the Register method
    
    * Adding DTO for user registration
    
    * Configuring debbuger
    
    * Adding validation for Username and Password
    
    * Creating Login method in API
    
    * Adding Authentication Middleware
    
    * Changing git ignore
    
    * Adding navbar
    
    Creating the Navigation and Login form based an Jumbotron Bootstrap example
    
    * Angular template form for username and password
    
    Angular template form for username and password
    
    * Adding auth service
    
    * Injecting Angulatr service in Component
    
    * Top panel logged in/out
    
    Using *ngif to conditionally display HTML elements in Navbar depending on logging state
    
    * Adding components for register
    
    Adding home and register componenets
    
    * Parent to child communication exercise
    
    Parent to child communication exercise
    
    * Making Cancel Register button work
    
    Making Cancel Register button work
    
    * Adding register method
    
    Adding register method to the Auth Service
    
    * Cleaning code
    
    Cleaning code from unnecessary handling Values objects

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -24,6 +24,7 @@ namespace DatingApp.API.Controllers
 
         // GET api/values
         //public ActionResult<IEnumerable<string>> Get()
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
