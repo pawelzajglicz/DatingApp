6b05b0fcf09815480c5482d62bf6ec8b6d659900
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-12 21:43:25 +0100

    Removing Elvis operator
    Removing elvis operator - now data is loaded before routing and Elvis operator is no more needed

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { User } from '../../_models/user';
 import { AlertifyService } from '../../_services/alertify.service';
 import { UserService } from '../../_services/user.service';
@@ -12,18 +13,21 @@ export class MemberListComponent implements OnInit {
   users: User[];
 
   constructor(private userService: UserService,
-              private alertify: AlertifyService) { }
+              private alertify: AlertifyService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.loadUsers();
+    this.route.data.subscribe(data => {
+      this.users = data['users'];
+    })
   }
 
-  loadUsers() {
+  /*loadUsers() {
     this.userService.getUsers().subscribe((users: User[]) => {
       this.users = users;
     }, error => {
       this.alertify.error(error);
     });
-  }
+  }*/
 
 }
