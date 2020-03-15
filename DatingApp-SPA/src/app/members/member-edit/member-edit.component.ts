52c8f994c5efc5fac872a088e6bb5c933d5a5400
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-15 20:41:34 +0100

    Adding notification for User about unsaved changes in Edit form

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/_models/user';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/_models/user';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -9,14 +11,24 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberEditComponent implements OnInit {
 
+  @ViewChild('editForm', {static: true})
+  editForm: NgForm;
+
   user: User;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile updated successfully');
+    this.editForm.reset(this.user);
   }
 
 }
