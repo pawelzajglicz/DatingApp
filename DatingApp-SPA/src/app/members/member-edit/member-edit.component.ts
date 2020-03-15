77ea4af6e65817a643a558d36211b691f3bc703f
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-03-15 22:28:15 +0100

    Adding Update User functionallity in Frontend
    Creating updateUser method in UserService and using it in MemberEditComponent

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -3,6 +3,8 @@ import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/user';
 import { AlertifyService } from 'src/app/_services/alertify.service';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -25,7 +27,9 @@ export class MemberEditComponent implements OnInit {
 
 
   constructor(private route: ActivatedRoute,
-              private alertify: AlertifyService) { }
+              private alertify: AlertifyService,
+              private userService: UserService,
+              private authService: AuthService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -34,9 +38,12 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateUser() {
-    console.log(this.user);
-    this.alertify.success('Profile updated successfully');
-    this.editForm.reset(this.user);
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
+      this.alertify.success('Profile updated successfully');
+      this.editForm.reset(this.user);
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
