274aa18d972ae080cc6e86a0d56c669b9b49d97a
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-04-11 22:55:30 +0200

    Configuring application

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -88,7 +88,15 @@ namespace DatingApp.API
             //app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new {controller = "Fallback", action = "Index"}
+                );
+            });
         }
     }
 }
